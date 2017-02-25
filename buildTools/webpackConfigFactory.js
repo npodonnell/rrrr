68bd5118dcd224c3d187bccd1f595a81e60ce3ac
Noel <odonnell.noel@gmail.com>
2017-02-25 16:17:15 +0000

    basic react app functioning

--- buildTools/webpackConfigFactory.js
+++ buildTools/webpackConfigFactory.js
@@ -5,11 +5,25 @@ const path = require('path')
 module.exports = (basePath, prod) => {
 	return {
 		entry: [
-			path.resolve(basePath, 'src/index.js')
+			path.resolve(basePath, 'src/index.jsx')
 		],
 		output: {
 			path: path.resolve(basePath, 'dist'),
 			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					include: path.resolve(basePath, 'src'),
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['react']
+						}
+					}
+				}
+			]
 		}
 	}
 }
