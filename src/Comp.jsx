6221591d30fa4c4d678e5138b6d54a179401dc72
Noel <odonnell.noel@gmail.com>
2017-06-05 22:09:32 +0100

    removed test code

--- src/Comp.jsx
+++ src/Comp.jsx
@@ -5,12 +5,6 @@ import React from 'react'
 export default class Comp extends React.Component {
 
 	render() {
-
-		setTimeout(() => {
-			x = y + 4
-			alert(x)
-		}, 10000)
-
 		return <div><b>This is a component.... Edit Comp.js and save and watch us update!</b></div>
 	}
 }
